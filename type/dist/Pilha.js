472716ed70c1387459ccc6794e7073b9ed69b38b
sofia <sofialvestoreti@gmail.com>
2025-04-30 22:19:55 -0300

    fim da pilha kkk

--- type/dist/Pilha.js
+++ type/dist/Pilha.js
@@ -27,8 +27,14 @@ class ArrayStack {
 const stack = new ArrayStack();
 stack.push(1);
 stack.push(2);
+console.log('size (2): ', stack.size());
+stack.clear();
+console.log('size (0): ', stack.size());
+stack.push(1);
+stack.push(2);
 stack.push(3);
 stack.push(4);
+console.log('top: ', stack.top());
 while (!stack.isEmpty()) {
-    console.log(stack.pop());
+    console.log('pop: ', stack.pop());
 }
