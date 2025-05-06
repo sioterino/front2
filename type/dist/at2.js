95c05ae5fbf00b5aff17f3c48150a780ee1bcc56
sofia <sofialvestoreti@gmail.com>
2025-05-05 21:30:34 -0300

    comandos

---
+++ type/dist/at2.js
@@ -0,0 +1,15 @@
+"use strict";
+/**
+ *          1. crie uma função genérica que retorne o primeiro elemento de um array de tipo qualquer
+ **/
+/**
+ *          2. crie uma função ecibir() com sobrecarga que receba um número ou string e retorne uma mensagem diferente para cada tipo
+ **/
+/**
+ *          3. crie uma função sobrecarregada combinar que:
+ *              - some doi números, ou
+ *              - concatene duas strings
+ **/
+/**
+ *          4. converta as funções do exercício de programação funcional com JavaScript em TypeScript
+ **/
